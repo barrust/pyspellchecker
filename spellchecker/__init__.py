c3d11cbe08c0a98c4625d8f8e28ca4aef68082e0
Tyler Barrus <barrust@gmail.com>
2019-01-29 07:50:30 -0500

    black formatting (#30)
    https://github.com/ambv/black

--- spellchecker/__init__.py
+++ spellchecker/__init__.py
@@ -1,7 +1,15 @@
-''' SpellChecker Module '''
-from . spellchecker import SpellChecker, WordFrequency
-from . info import (__author__, __maintainer__, __email__, __license__,   # noqa: F401
-                    __version__, __credits__, __url__, __bugtrack_url__)  # noqa: F401
+""" SpellChecker Module """
+from .spellchecker import SpellChecker, WordFrequency
+from .info import (
+    __author__,
+    __maintainer__,
+    __email__,
+    __license__,  # noqa: F401
+    __version__,
+    __credits__,
+    __url__,
+    __bugtrack_url__,
+)  # noqa: F401
 
 
-__all__ = ['SpellChecker', 'WordFrequency']
+__all__ = ["SpellChecker", "WordFrequency"]
--- spellchecker/info.py
+++ spellchecker/info.py
@@ -1,11 +1,11 @@
-''' SpellChecker Information '''
+""" SpellChecker Information """
 
 
-__author__ = 'Tyler Barrus'
-__maintainer__ = 'Tyler Barrus'
-__email__ = 'barrust@gmail.com'
-__license__ = 'MIT'
-__version__ = '0.3.0'
-__credits__ = ['Peter Norvig']
-__url__ = 'https://github.com/barrust/pyspellchecker'
-__bugtrack_url__ = '{0}/issues'.format(__url__)
+__author__ = "Tyler Barrus"
+__maintainer__ = "Tyler Barrus"
+__email__ = "barrust@gmail.com"
+__license__ = "MIT"
+__version__ = "0.3.0"
+__credits__ = ["Peter Norvig"]
+__url__ = "https://github.com/barrust/pyspellchecker"
+__bugtrack_url__ = "{0}/issues".format(__url__)
