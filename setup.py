a7862cb2962a820664988cb8458736a81e7dcb02
Tyler Barrus <barrust@gmail.com>
2019-02-05 20:54:59 -0500

    update tests to include python2

--- setup.py
+++ setup.py
@@ -42,11 +42,12 @@ setup(
         'License :: OSI Approved',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     test_suite = 'tests'
 )
