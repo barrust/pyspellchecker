bca57e12988642e7790c7298518cf1640767b03d
sviperm <unsviperm@gmail.com>
2021-03-17 23:02:49 +0300

    Add 🇷🇺 pipeline

--- scripts/cleaner/__init__.py
+++ scripts/cleaner/__init__.py
@@ -1,22 +1,19 @@
 import os
 import traceback
 
-from .pipelines import (BasePipeline, EnglishPipeline, FrenchPipeline,
-                        GermanPipeline, PortuguesePipeline, RussianPipeline,
-                        SpanishPipeline)
+from pipelines import (BasePipeline, EnglishPipeline, FrenchPipeline,
+                       GermanPipeline, PortuguesePipeline, RussianPipeline,
+                       SpanishPipeline)
 
 
 class Cleaner:
     script_path = os.path.dirname(os.path.abspath(__file__))
-    module_path = os.path.abspath("{}/../../".format(script_path))
+    module_path = os.path.abspath("{}/../".format(script_path))
     resources_path = os.path.abspath("{}/resources/".format(module_path))
-    data_path = os.path.abspath("{}/data/".format(script_path))
+    data_path = os.path.abspath("{}/data/".format(module_path))
 
     def __init__(self, language) -> None:
         self.language = language
-        self.exclude_filepath = os.path.abspath("{}/{}_exclude.txt".format(self.data_path, language))
-        self.include_filepath = os.path.abspath("{}/{}_include.txt".format(self.data_path, language))
-        self.misfit_filepath = os.path.abspath("{}/{}_misfit.txt".format(self.data_path, language))
 
         self.pipeline = self._get_pipeline(language)
 
@@ -37,7 +34,93 @@ class Cleaner:
             raise Exception("{} language is not supported.\n"
                             "Please, use one this".format(language, langs))
         # TODO: init pipeline
-        return lang_pipeline()
+        exclude_path = os.path.abspath("{}/{}_exclude.txt".format(self.data_path, language))
+        include_path = os.path.abspath("{}/{}_include.txt".format(self.data_path, language))
+        misfit_path = os.path.abspath("{}/{}_misfit.txt".format(self.data_path, language))
+        return lang_pipeline(exclude_path, include_path, misfit_path)
 
     def clean(self, word_frequency):
         return self.pipeline(word_frequency)
+
+
+if __name__ == '__main__':
+    wf = {
+        "α3β4-рецептора": 1,
+        "β": 1,
+        "β-hgs": 1,
+        "β-klotho": 4,
+        "β-адренэргический": 1,
+        "β-адренэргического": 1,
+        "β-амилоиды": 1,
+        "β-гидроксибутират": 3,
+        "β-гидроксибутирата": 2,
+        "β-гидроксибутиратом": 1,
+        "β-гидроксилаза": 2,
+        "β-гидроксилазы": 2,
+        "β-каротин": 1,
+        "β-каротина": 1,
+        "β-катенин": 1,
+        "β-кетокислоты": 1,
+        "β-криптоксантин": 1,
+        "β2pkc": 1,
+        "ε": 1,
+        "κυπε": 1,
+        "λ": 1,
+        "λb": 2,
+        "λcdm": 6,
+        "λcdm-модель": 1,
+        "μ": 1,
+        "μ-опиоидные": 1,
+        "μετεμψύχωσις": 1,
+        "π": 1,
+        "προς": 2,
+        "ρικ": 1,
+        "ρώσους": 2,
+        "σ_σ": 1,
+        "σb": 3,
+        "ψ-мезон": 2,
+        "а": 30849,
+        "а-100лл": 8,
+        "а-10c": 1,
+        "а-10а": 4,
+        "а-10с": 7,
+        "а-18e": 1,
+        "а-190э": 8,
+        "а-192м": 14,
+        "а-1м": 3,
+        "а-20g": 1,
+        "а-220м": 4,
+        "а-319-115xcj": 3,
+        "а-350р": 1,
+        "а-35м": 2,
+        "а-400м": 3,
+        "а-50и": 1,
+        "а-50м": 3,
+        "а-50у": 47,
+        "а-50э": 1,
+        "а-50эи": 21,
+        "а-74т-200а": 1,
+        "а-7а": 2,
+        "а-day": 1,
+        "а-ha": 1,
+        "а-mobile": 2,
+        "а-si": 1,
+        "а-а-а": 3,
+        "а-а-а-а-ах": 1,
+        "а-а-а-апчихуа": 1,
+        "а-аа": 5,
+        "а-ав": 5,
+        "а-асара": 2,
+        "а-б": 5,
+        "а-брендами": 1,
+        "а-вым": 1,
+        "а-гай": 1,
+        "а-групп": 5,
+        "а-дин": 11,
+        "а-дура": 2,
+        "а-дуры": 1,
+    }
+    cleaner = Cleaner('ru')
+    mf = cleaner.clean(wf)
+    # dump wf
+    # dump mf
--- scripts/cleaner/pipelines.py
+++ scripts/cleaner/pipelines.py
@@ -1,6 +1,55 @@
-class BasePipeline:
+from abc import abstractmethod, ABCMeta
+from build_dictionary import load_file
+
+
+class BasePipeline(metaclass=ABCMeta):
+    MINIMUM_FREQUENCY = 15
+
+    @abstractmethod
+    def __init__(self, exclude_path, include_path, misfit_path, *args) -> None:
+        self.exclude_path = exclude_path
+        self.include_path = include_path
+        self.misfit_path = misfit_path
+        self.pipeline = (*args,
+                         self.remove_small_frequency,
+                         self.remove_from_exclude_file,
+                         self.add_from_include_file)
+
     def __call__(self, word_frequency):
-        return word_frequency
+        misfits = []
+        for pipe in self.pipeline:
+            misfit = pipe(word_frequency)
+            if misfit:
+                misfits += misfit
+        return misfits
+
+    def remove_small_frequency(self, word_frequency):
+        # remove small numbers
+        invalid_words = list()
+        for word in word_frequency:
+            if word_frequency[word] <= self.MINIMUM_FREQUENCY:
+                invalid_words.append(word)
+        for invalid_word in invalid_words:
+            word_frequency.pop(invalid_word)
+        return invalid_words
+
+    def remove_from_exclude_file(self, word_frequency):
+        # remove flagged misspellings
+        with load_file(self.exclude_path) as fobj:
+            for word in fobj:
+                word = word.strip()
+                if word in word_frequency:
+                    word_frequency.pop(word)
+
+    def add_from_include_file(self, word_frequency):
+        # # Add known missing words back in (ugh)
+        with load_file(self.include_path) as fobj:
+            for word in fobj:
+                word = word.strip()
+                if word in word_frequency:
+                    print("{} is already found in the dictionary! Skipping!")
+                else:
+                    word_frequency[word] = self.MINIMUM_FREQUENCY
 
 
 class EnglishPipeline(BasePipeline):
@@ -24,4 +73,59 @@ class PortuguesePipeline(BasePipeline):
 
 
 class RussianPipeline(BasePipeline):
-    pass
+    def __init__(self, exclude_path, include_path, misfit_path) -> None:
+        super().__init__(exclude_path, include_path, misfit_path,
+                         self.remove_words_with_invalid_chars,
+                         self.remove_words_without_vowel,
+                         self.remove_ellipses,
+                         self.remove_doubles)
+
+    @classmethod
+    def remove_words_with_invalid_chars(cls, word_frequency):
+        valid_letters = set("абвгдеёжзийклмнопрстуфхцчшщъыьэюя-")
+        invalid_words = list()
+        for word in word_frequency:
+            letters = set(word)
+            if not letters.issubset(valid_letters):
+                invalid_words.append(word)
+        for invalid_word in invalid_words:
+            word_frequency.pop(invalid_word)
+        return invalid_words
+
+    @classmethod
+    def remove_words_without_vowel(cls, word_frequency):
+        # remove words without a vowel
+        invalid_words = list()
+        vowels = set("аеёиоуыэюя")
+        for word in word_frequency:
+            if word in ['ржд']:
+                continue
+            if vowels.isdisjoint(word):
+                invalid_words.append(word)
+        for invalid_word in invalid_words:
+            word_frequency.pop(invalid_word)
+        return invalid_words
+
+    @classmethod
+    def remove_ellipses(cls, word_frequency):
+        # remove ellipses
+        invalid_words = list()
+        for word in word_frequency:
+            if ".." in word:
+                invalid_words.append(word)
+        for invalid_word in invalid_words:
+            word_frequency.pop(invalid_word)
+        return invalid_words
+
+    @classmethod
+    def remove_doubles(cls, word_frequency):
+        # leading or trailing doubles a, "a'", "zz", ending y's
+        invalid_words = list()
+        for word in word_frequency:
+            if word.startswith("аа") and (word not in ("аарон", "аарона", "аарону")):
+                invalid_words.append(word)
+            elif word.startswith("ээ") and (word not in ("ээг")):
+                invalid_words.append(word)
+        for invalid_word in invalid_words:
+            word_frequency.pop(invalid_word)
+        return invalid_words
