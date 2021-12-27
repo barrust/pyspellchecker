ce0777fcea76cdfb6d985ab9f45affb65b8c2c71
Tyler Barrus <barrust@gmail.com>
2021-12-26 19:40:28 -0500

    first typing

--- spellchecker/utils.py
+++ spellchecker/utils.py
@@ -3,17 +3,21 @@ import contextlib
 import gzip
 import functools
 import re
+import typing
 import warnings
 
 from .info import __version__
 
 
-def fail_after(version):
-    """ Decorator to add to tests to ensure that they fail if a deprecated
-        feature is not removed before the specified version
+KeyT = typing.Union[str, bytes]
 
-        Args:
-            version (str): The version to check against """
+
+def fail_after(version: str) -> typing.Callable:
+    """Decorator to add to tests to ensure that they fail if a deprecated
+    feature is not removed before the specified version
+
+    Args:
+        version (str): The version to check against"""
 
     def decorator_wrapper(func):
         @functools.wraps(func)
@@ -32,12 +36,12 @@ def fail_after(version):
     return decorator_wrapper
 
 
-def deprecated(message=""):
-    """ A simplistic decorator to mark functions as deprecated. The function
-        will pass a message to the user on the first use of the function
+def deprecated(message: str = "") -> typing.Callable:
+    """A simplistic decorator to mark functions as deprecated. The function
+    will pass a message to the user on the first use of the function
 
-        Args:
-            message (str): The message to display if the function is deprecated
+    Args:
+        message (str): The message to display if the function is deprecated
     """
 
     def decorator_wrapper(func):
@@ -59,15 +63,15 @@ def deprecated(message=""):
     return decorator_wrapper
 
 
-def ensure_unicode(_str, encoding="utf-8"):
-    """ Simplify checking if passed in data are bytes or a string and decode
-        bytes into unicode.
+def ensure_unicode(_str: KeyT, encoding: str = "utf-8") -> str:
+    """Simplify checking if passed in data are bytes or a string and decode
+    bytes into unicode.
 
-        Args:
-            _str (str): The input string (possibly bytes)
-            encoding (str): The encoding to use if input is bytes
-        Returns:
-            str: The encoded string
+    Args:
+        _str (str): The input string (possibly bytes)
+        encoding (str): The encoding to use if input is bytes
+    Returns:
+        str: The encoded string
     """
     if isinstance(_str, bytes):
         return _str.decode(encoding)
@@ -75,7 +79,9 @@ def ensure_unicode(_str, encoding="utf-8"):
 
 
 @contextlib.contextmanager
-def __gzip_read(filename, mode="rb", encoding="UTF-8"):
+def __gzip_read(
+    filename: str, mode: str = "rb", encoding: str = "UTF-8"
+) -> typing.Generator[KeyT, None, None]:
     """ Context manager to correctly handle the decoding of the output of \
         the gzip file
 
@@ -91,15 +97,15 @@ def __gzip_read(filename, mode="rb", encoding="UTF-8"):
 
 
 @contextlib.contextmanager
-def load_file(filename, encoding):
-    """ Context manager to handle opening a gzip or text file correctly and
-        reading all the data
+def load_file(filename: str, encoding: str) -> typing.Generator[KeyT, None, None]:
+    """Context manager to handle opening a gzip or text file correctly and
+    reading all the data
 
-        Args:
-            filename (str): The filename to open
-            encoding (str): The file encoding to use
-        Yields:
-            str: The string data from the file read
+    Args:
+        filename (str): The filename to open
+        encoding (str): The file encoding to use
+    Yields:
+        str: The string data from the file read
     """
     if filename[-3:].lower() == ".gz":
         with __gzip_read(filename, mode="rt", encoding=encoding) as data:
@@ -109,15 +115,15 @@ def load_file(filename, encoding):
             yield fobj.read()
 
 
-def write_file(filepath, encoding, gzipped, data):
-    """ Write the data to file either as a gzip file or text based on the
-        gzipped parameter
+def write_file(filepath: str, encoding: str, gzipped: bool, data: str) -> None:
+    """Write the data to file either as a gzip file or text based on the
+    gzipped parameter
 
-        Args:
-            filepath (str): The filename to open
-            encoding (str): The file encoding to use
-            gzipped (bool): Whether the file should be gzipped or not
-            data (str): The data to be written out
+    Args:
+        filepath (str): The filename to open
+        encoding (str): The file encoding to use
+        gzipped (bool): Whether the file should be gzipped or not
+        data (str): The data to be written out
     """
     if gzipped:
         with gzip.open(filepath, "wt") as fobj:
@@ -127,12 +133,12 @@ def write_file(filepath, encoding, gzipped, data):
             fobj.write(data)
 
 
-def _parse_into_words(text):
-    """ Parse the text into words; currently removes punctuation except for
-        apostrophies.
+def _parse_into_words(text: str) -> typing.List[str]:
+    """Parse the text into words; currently removes punctuation except for
+    apostrophies.
 
-        Args:
-            text (str): The text to split into words
+    Args:
+        text (str): The text to split into words
     """
     # see: https://stackoverflow.com/a/12705513
     return re.findall(r"(\w[\w']*\w|\w)", text)
