e6c1d3686883c3a68f9102d9af955cdeaec68b14
barrust <barrust@gmail.com>
2024-12-26 12:58:50 -0500

    another test

--- spellchecker/__init__.py
+++ spellchecker/__init__.py
@@ -4,12 +4,12 @@ from spellchecker.info import (
     __author__,
     __maintainer__,
     __email__,
-    __license__,  # noqa: F401
+    __license__,
     __version__,
     __credits__,
     __url__,
-    __bugtrack_url__,
-)  # noqa: F401
+    __bugtrack_url__
+)
 
 
 __all__ = ["SpellChecker", "WordFrequency"]
